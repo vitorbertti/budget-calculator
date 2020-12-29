cfc9545736cb363ab0daa0de37dbd0f67007315a
Vitor Bertti <berttivitor@gmail.com>
2020-12-28 22:25:54 -0300

    Added delete button

--- src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
+++ src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-budget-item-card',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BudgetItemCardComponent implements OnInit {
 
+  @Input() isInome: boolean = true;
   constructor() { }
 
   ngOnInit(): void {
