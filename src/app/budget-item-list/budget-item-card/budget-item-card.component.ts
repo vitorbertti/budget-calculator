7b3356a53f1d5015f68f79d87b655dfc82fd4371
Vitor Bertti <berttivitor@gmail.com>
2021-01-04 21:32:47 -0300

    Added delete function

--- src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
+++ src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { BudgetItem } from 'src/shared/models/budget-item-model';
 
 @Component({
@@ -9,9 +9,14 @@ import { BudgetItem } from 'src/shared/models/budget-item-model';
 export class BudgetItemCardComponent implements OnInit {
 
   @Input() item: BudgetItem;
+  @Output() xButtonClick : EventEmitter<any> = new EventEmitter<any>();
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onXButtonClick() {
+    this.xButtonClick.emit();
   }
 
 }
