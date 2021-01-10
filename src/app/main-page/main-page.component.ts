0e83fbcdccd19f431023d738645c98d156186c4a
Vitor Bertti <berttivitor@gmail.com>
2021-01-10 15:10:05 -0300

    Added updateItem function

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UpdateEvent } from '../budget-item-list/budget-item-list.component';
 import { BudgetItem } from 'src/shared/models/budget-item-model';
 
 @Component({
@@ -25,6 +26,13 @@ export class MainPageComponent implements OnInit {
     let index = this.budgetItems.indexOf(item);
     this.budgetItems.splice(index, 1);
     this.totalBudget -= item.amount;
+  }
+
+  updateItem(updateEvent: UpdateEvent) {
+    this.budgetItems[this.budgetItems.indexOf(updateEvent.old)] = updateEvent.new;
+
+    this.totalBudget -= updateEvent.old.amount;
+    this.totalBudget += updateEvent.new.amount;
   }
 
 }
